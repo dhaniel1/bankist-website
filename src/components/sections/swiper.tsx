17515a498dc77d4d7b279a3b05836551fb935ed6
tundeLayan <olatunde@kreatesell.com>
2023-05-15 16:35:36 +0100

    tunde

--- src/components/sections/swiper.tsx
+++ src/components/sections/swiper.tsx
@@ -9,18 +9,20 @@ import "pure-react-carousel/dist/react-carousel.es.css";
 
 function SwiperComponent() {
   return (
+    <div>
     <CarouselProvider
       naturalSlideWidth={10}
       naturalSlideHeight={12.5}
       totalSlides={3}>
       <Slider>
-        <Slide index={0}>I am the first Slide.</Slide>
-        <Slide index={1}>I am the second Slide.</Slide>
-        <Slide index={2}>I am the third Slide.</Slide>
+        <Slide style={{border: '3px solid blue', height: '350px'}} index={0}>I am the first Slide.</Slide>
+        <Slide style={{border: '3px solid blue', height: '350px'}} index={1}>I am the second Slide.</Slide>
+        <Slide style={{border: '3px solid blue', height: '350px'}} index={2}>I am the third Slide.</Slide>
       </Slider>
-      <ButtonBack>Back</ButtonBack>
-      <ButtonNext>Next</ButtonNext>
+      <ButtonBack style={{height: '300px', position: 'absolute', background: 'red', top: 0}}>Back</ButtonBack>
+      <ButtonNext style={{height: '300px', position: 'absolute', background: 'red', top: 0, right: 0}}>Next</ButtonNext>
     </CarouselProvider>
+    </div>
   );
 }
 export default SwiperComponent;
